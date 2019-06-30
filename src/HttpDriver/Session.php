00e8c823f944d9db43d5f1b37f9eb6e87b197c26
Artur Cichosz <artur2000@gmx.de>
2019-06-30 22:29:04 +0200

    fixed boolean type http header which is appearently not supported by the guzzle http client anymore (Header value must be a string or numeric but boolean provided)

--- src/HttpDriver/Session.php
+++ src/HttpDriver/Session.php
@@ -192,7 +192,7 @@ class Session implements SessionInterface
         ]);
         $headers = [
             [
-                'X-Stream' => true,
+                'X-Stream' => 'true',
                 'Content-Type' => 'application/json',
             ],
         ];
